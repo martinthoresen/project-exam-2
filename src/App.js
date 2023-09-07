218c01cb5a2a20b93af69a4490c5a5a1772828f2
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-09-07 05:00:51 +0200

    update routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import Layout from "./components/Layout";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
+import Venues from "./components/Venues";
 
 function App() {
-  return <Layout />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Venues />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
