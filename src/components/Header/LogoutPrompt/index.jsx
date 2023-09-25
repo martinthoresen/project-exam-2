f98608b9fb91d4e57d66ef5cd0d4b20f54359b91
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-09-25 18:39:40 +0200

    create a logout prompt modal

---
+++ src/components/Header/LogoutPrompt/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+function LogoutPrompt() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Log Out
+      </Button>
+
+      <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Log Out</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to log out?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger">Log out</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default LogoutPrompt;
