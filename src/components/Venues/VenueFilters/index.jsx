afb707e71cce75e2c180dba1a5ccdca509a47c01
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-09-27 00:39:07 +0200

    Update index.jsx

--- src/components/Venues/VenueFilters/index.jsx
+++ src/components/Venues/VenueFilters/index.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { Button, Form, InputGroup, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, InputGroup, Row, Stack } from "react-bootstrap";
 
 function VenueFilters() {
   return (
     <Form>
       <Row>
-        <InputGroup className="mb-3">
+        <InputGroup as={Col} md="Row">
           <Form.Control placeholder="Ex. Condo, Beach" aria-label="Search Venues" />
           <Button variant="outline-secondary" id="button-search">
             Search
           </Button>
         </InputGroup>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-3" as={Col}>
+          <Form.Label>Guests:</Form.Label>
+          <Form.Control type="number" min={1} />
+        </Form.Group>
+        <Form.Group className="mb-3" as={Col}>
           <Form.Label>Guests:</Form.Label>
           <Form.Control type="number" />
         </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Guests:</Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
-        <Form.Group>
+        <Form.Group as={Col} className="d-flex m-auto ">
           <Form.Check type="switch" id="pets-switch" label="Pets" />
+          <Form.Check type="switch" id="wifi-switch" label="Wifi" />
+          <Form.Check type="switch" id="parking-switch" label="Parking" />
+          <Form.Check type="switch" id="breakfast-switch" label="Breakfast" />
         </Form.Group>
       </Row>
     </Form>
