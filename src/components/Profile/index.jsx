7d215fdb3bf05111353b58a9db3094287e8ec0c4
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-09-27 21:00:16 +0200

    Update index.jsx

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Col, Container, Nav, Navbar, Row, Stack } from "react-bootstrap";
+import useApi from "../../hooks/useApi";
+import baseUrl from "../../utility/constants/baseUrl";
+import { loadKey } from "../../storage/localStorage";
 
 function Profile() {
+  const userCreds = loadKey("data");
+  const userName = userCreds["name"];
+  const userData = useApi(baseUrl + `/holidaze/profiles/${userName}`);
+  console.log(userData.data);
   return (
     <div>
       <Row>
@@ -9,7 +16,7 @@ function Profile() {
           <Container className="my-5 text-center">
             <img src="" alt="" srcset="" />
             <div>
-              <p>USERNAME</p>
+              <p>{userData.data.name}</p>
               <p>Venue Manager</p>
             </div>
           </Container>
