7de10054b6aff8e47b885cad7a5cb1d9174453e6
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-09-27 21:00:20 +0200

    Update useApi.js

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -6,7 +6,7 @@ import { loadKey } from "../storage/localStorage";
  */
 
 function useApi(url) {
-  // const token = loadKey("accessToken");
+  const token = loadKey("accessToken");
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -16,13 +16,13 @@ function useApi(url) {
       try {
         setIsLoading(true);
         setIsError(false);
-        /* const headerData = {
+        const headerData = {
           method: "GET",
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        }; */
-        const fetchedData = await fetch(url /*headerData */);
+        };
+        const fetchedData = await fetch(url, headerData);
         const json = await fetchedData.json();
         setData(json);
       } catch (error) {
