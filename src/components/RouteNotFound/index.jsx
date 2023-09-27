d1916c27ac25daf22be784714ac348a0c1c7fe5d
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-09-27 16:19:28 +0200

    update margin to fill screen on all devices

--- src/components/RouteNotFound/index.jsx
+++ src/components/RouteNotFound/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function RouteNotFound() {
   return (
-    <Container>
+    <Container className="m-auto">
       <Col>
         <Row>
           <h1 className="text-center">Error</h1>
           <p className="text-center">Oops... Could not find the page you're looking for.</p>
+          <Link to="/" className="text-center text-primary">
+            Return to Home
+          </Link>
         </Row>
       </Col>
     </Container>
