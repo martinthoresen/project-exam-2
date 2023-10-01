1bd66b2e3c8fe9c662b8877d7e9638d3bc9cf12b
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-10-01 18:52:42 +0200

    update register to give feedback to user if error

--- src/auth/registerUser.js
+++ src/auth/registerUser.js
@@ -1,4 +1,7 @@
+import { displayMessage } from "../utility/displayMessage";
+
 async function registerUser(url, userData) {
+  const registerContainer = document.querySelector("#register-container");
   try {
     const postData = {
       method: "POST",
@@ -9,7 +12,15 @@ async function registerUser(url, userData) {
     };
 
     const response = await fetch(url, postData);
-    console.log(response);
+    const json = await response.json();
+
+    if (response.ok) {
+      displayMessage(registerContainer, "Success! You can now log in.", "success");
+      window.location.replace("/login");
+    } else {
+      console.log(response);
+      displayMessage(registerContainer, json.errors[0].message, "danger");
+    }
   } catch (error) {
     console.log(error);
   }
