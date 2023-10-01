5160786706ed8f85b4774d6ebeef4f373469569c
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-10-01 18:54:12 +0200

    added render venues component

--- src/components/Venues/RenderVenues/index.jsx
+++ src/components/Venues/RenderVenues/index.jsx
@@ -4,13 +4,42 @@ import useApi from "../../../hooks/useApi";
 import baseUrl from "../../../utility/constants/baseUrl";
 import Spinner from "react-bootstrap/Spinner";
 
-function RenderVenues() {
-  let limit = 24;
-  let offset = 0;
-
-  const { data, isLoading, isError } = useApi(baseUrl + `/holidaze/venues?limit=${limit}&offset=${offset}`);
-
+function RenderVenues(props) {
+  const { data, isLoading, isError } = useApi(baseUrl + `/holidaze/venues`);
   const venues = data;
+
+  if (props) {
+    const searchValue = props.searchValue;
+    const guestsValue = props.guestsValue;
+    console.log(venues);
+    const filteredVenues = venues.filter((venue) => venue.name.toLowerCase().includes(searchValue) || venue.location.country.toLowerCase().includes(searchValue));
+    return (
+      <Row>
+        {filteredVenues.map((venue) => (
+          <Col className="col-12 col-sm-6 col-lg-3" key={venue.id}>
+            <a href={`/venue/?id=${venue.id}`} className="text-decoration-none">
+              <Card className="border-0 shadow-sm mb-3 rounded">
+                <Card.Body>
+                  <Card.Img variant="top" src={venue.media} className="rounded card-img" onError={(event) => (event.target.src = "/images/no-image.jpg")} />
+                  <Card.Title>{venue.name}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-primary">
+                    ${venue.price} <span className="text-muted small">/night</span>
+                  </Card.Subtitle>
+                  <div className="d-flex justify-content-between align-items-end">
+                    <Button variant="outline-primary">View</Button>
+                    <Card.Text className=" text-muted">
+                      Rating: <span className="text-primary">{venue.rating}</span>
+                    </Card.Text>
+                  </div>
+                </Card.Body>
+              </Card>
+            </a>
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+
   if (isLoading) {
     return (
       <Row>
